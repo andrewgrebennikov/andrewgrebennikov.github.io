2b94f16ddb2632422dc7fd2d4456ab0937ed5447
andrewgrebennikov <lostplanet08@mail.ru>
2017-11-28 16:50:08 +0400

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.slider__list').slick({
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></button>',
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i></button>'
+    });
+
+    $('.tabs__controls-link').on('click', function (e) {
+        e.preventDefault();
+
+        var item = $(this).closest('.tabs__controls-item'),
+            contentItem = $('.tabs__item'),
+            itemPosition = item.data('class');
+
+        contentItem.filter('.tabs__item_' + itemPosition)
+            .add(item)
+            .addClass('active')
+            .siblings()
+            .removeClass('active')
+    })
+});
